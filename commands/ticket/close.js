45f3b97e49bde3a06ccb50fd587f8c828f8446f1
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2021-03-21 19:59:49 +0100

    Update close.js

--- commands/ticket/close.js
+++ commands/ticket/close.js
@@ -9,7 +9,8 @@ module.exports = {
     'close',
     'ct',
   ],
-    callback: (message, args) => {
+  
+  run: (client, message, args) => {
         if(!message.channel.name.includes('ticket')) return message.reply('You Cant Delete A Normal Channel.') // If Non Ticket Channel Is Tried To Delete
         message.channel.delete()
     }
