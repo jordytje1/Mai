cd14cab4f88bd854c3e6388593d701986b164c4c
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2021-03-21 19:59:02 +0100

    Update close.js

--- commands/ticket/close.js
+++ commands/ticket/close.js
@@ -1,9 +1,14 @@
 module.exports = {
-    commands: ['close-ticket', 'ct'], // You Can Keep Any Name
-    permissions: 'MANAGE_CHANNELS', // You Can Keep Any Permission
-    permissionError: 'You Cant Use This Command', // Optional
-    description: 'Close A Opened Ticket', // Optional
-
+  name: 'close',
+  aliases: [ 'ct', 'close' ],
+  group: 'ticket',
+  description: 'Sends a list of all commands from each specific command groups',
+  clientPermissions: [ 'EMBED_LINKS' ],
+  parameters: [],
+  examples: [
+    'close',
+    'ct',
+  ],
     callback: (message, args) => {
         if(!message.channel.name.includes('ticket')) return message.reply('You Cant Delete A Normal Channel.') // If Non Ticket Channel Is Tried To Delete
         message.channel.delete()
