1055a2685ea6073ddfcb1a24c84055922a2556a4
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2021-03-21 20:34:17 +0100

    Update react.js

--- events/react.js
+++ events/react.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = (client) => {
     client.on('messageReactionAdd', (reaction, user) => {
-        if(reaction.message.id === '810565609166602270' && reaction.emoji.name === '🎫') { // If Reaction Is Used In Message ID Provided // Emoji Should Be Same As Used In setup.js
+        if(reaction.message.id === '823277848722145360' && reaction.emoji.name === '🎫') { // If Reaction Is Used In Message ID Provided // Emoji Should Be Same As Used In setup.js
             reaction.users.remove(user)
 
             reaction.message.guild.channels.create(`ticket-${user.username}`, {
@@ -16,15 +16,15 @@ module.exports = (client) => {
                         deny: ['VIEW_CHANNEL']
                     },
                     { // For Mod, Admin etc... // Add As Many As You Like
-                        id: '806411060911276042',
+                        id: '376384142599782410',
                         allow: ['SEND_MESSAGES','VIEW_CHANNEL','MANAGE_MESSAGES','MANAGE_CHANNELS'] //Add As Many As You Like
                     },
                     { // For Mod, Admin etc...
-                        id: '806411060911276042',
+                        id: '376384142599782410',
                         allow: ['SEND_MESSAGES','VIEW_CHANNEL','MANAGE_MESSAGES','MANAGE_CHANNELS'] //Add As Many As You Like
                     }
                 ],
-                type : 'text', parent: '810399250336186398' // type:text For Text Channel and parent:810399250336186398 For In Which Category You Want the Ticket Channel To Be Created
+                type : 'text', parent: '823278205262888970' // type:text For Text Channel and parent:810399250336186398 For In Which Category You Want the Ticket Channel To Be Created
             }).then(async channel => {
                 channel.send(`<@${user.id}> Welcome!`, new MessageEmbed()
                 .setColor('RANDOM')
