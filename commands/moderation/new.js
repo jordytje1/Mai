19294817b3784c18676488d32699b98abebd8c36
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2021-03-23 15:36:37 +0100

    Update new.js

--- commands/moderation/new.js
+++ commands/moderation/new.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 const fs = require('fs');
-const config = require('/user/config.js');
+const config = require('user/config.js');
 
 module.exports = {
 	name: 'new',
