6b0d1b8f5a8a38c24106892a82105ad912f8b5f2
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2021-03-24 16:49:59 +0100

    Create warnings.js

---
+++ commands/moderation/warnings.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+module.exports = {
+	name: "warnings",
+	description: "Check the people you mentioned who has warnings or not",
+	usage: "d!warnings <mention>",
+	accessableby: "Members",
+	aliases: [],
+  
+ run: async (client, message, args) => {
+	const emddd = new Discord.MessageEmbed()
+		.setDescription(
+			"<:cross1:747728200691482746> **Couldn't find that user...!**"
+		)
+		.setColor("RED");
+
+	const warns = JSON.parse(
+		fs.readFileSync("./temp-datastore/warnings.json", "utf8")
+	);
+	const user = message.mentions.users.first();
+	if (message.mentions.users.size < 1) {
+		return message
+			.reply("You must mention someone to check their warns.")
+			.catch(console.error);
+	}
+	if (!user) return message.channel.send(emddd);
+	if (!warns[`${user.id}, ${message.guild.id}`]) {
+		warns[`${user.id}, ${message.guild.id}`] = {
+			warns: 0,
+		};
+	}
+	// if (!warns[user.id]) return message.channel.send(emddd)
+
+	const embed = new Discord.MessageEmbed()
+		.setColor("GREEN")
+		.setTimestamp()
+		.setTitle("<:tick:702386031361523723> Warn Check")
+		.addField("User:", `${user.username}#${user.discriminator}`)
+		.addField(
+			"Number of warnings:",
+			warns[`${user.id}, ${message.guild.id}`].warns
+		);
+	message.channel.send({ embed });
+}
+	}
+};
