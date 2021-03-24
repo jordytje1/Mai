9be3a24ea1bcf12c7e1deb2f36b427ae4efaee1d
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2021-03-24 20:40:19 +0100

    Create configuration.js

---
+++ configuration.js
@@ -0,0 +1,17 @@
+module.exports = {
+    TOKENS : {
+        DISCORD : ''
+    },
+    MONGOOSE : {
+        DBCONNECTION : 'mongodb+srv://hendrik:poepje000@cluster0.ybvrm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    },
+    DEFAULTSETTINGS: {
+        prefix: "!",
+        logChannel: "733994085106581547",
+        welcomeMessage: "welcome",
+        rankcard: "",
+        invitations : false, //Default anti-invitations status
+        kickauto : false //Default kick-auto status
+      },
+    }
+};
