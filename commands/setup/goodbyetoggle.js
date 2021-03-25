0b6dd203b5b1bf42d9008959a43caa78e6d45e84
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2021-03-25 18:14:07 +0100

    Update goodbyetoggle.js

--- commands/setup/goodbyetoggle.js
+++ commands/setup/goodbyetoggle.js
@@ -33,7 +33,7 @@ module.exports = {
       return message.channel.send(
         new MessageEmbed()
         .setColor('GREEN')
-        .setFooter(`Leaving Member Announcer | \©️${new Date().getFullYear()} Mai`)
+        .setFooter(`Leaving Member Announcer | \©️${new Date().getFullYear()} 𝕯𝖗𝖆𝖌𝖔𝖓𝖇𝖔𝖞#6241`)
         .setDescription([
           '<a:animatedcheck:758316325025087500>\u2000\u2000|\u2000\u2000',
           `Leaving Member Announcer Feature has been successfully **${state}**!\n\n`,
