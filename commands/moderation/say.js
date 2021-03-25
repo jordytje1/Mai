a078e55d7df5086915425103cd35db190b23476c
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2021-03-25 19:52:58 +0100

    Update say.js

--- commands/moderation/say.js
+++ commands/moderation/say.js
@@ -13,22 +13,25 @@ module.exports = {
         if (args.length < 1)
             return message.channel.send('You must specify something for the bot to repeat!').then(m => m.delete({timeout: 5000}));
             
-        if (args[0].toLowerCase() === 'embed') {
-            const embed = new MessageEmbed()
-                .setTitle('Message')
-                .setThumbnail(message.author.avatarURL())
-                .setColor("#30afe3")
-                .setDescription(args.slice(1).join(' '))
-                .setFooter(`Requested by ${message.author.tag} `)
-                .setTimestamp()
-            
-            if (args[0].toLowerCase() === 'normal embed') {
+         if (args[0].toLowerCase() === 'embed') {
             const embed = new MessageEmbed()
                 .setTitle('Message')
                 .setColor("#00c1ff")
                 .setDescription(args.slice(1).join(' '))
                 .setFooter(`Requested by ${message.author.tag} `)
                 .setTimestamp()
+        
+        
+        //if (args[0].toLowerCase() === 'embed') {
+            //const embed = new MessageEmbed()
+                //.setTitle('Message')
+                //.setThumbnail(message.author.avatarURL())
+                //.setColor("#30afe3")
+                //.setDescription(args.slice(1).join(' '))
+                //.setFooter(`Requested by ${message.author.tag} `)
+                //.setTimestamp()
+            
+          
             
             message.channel.send(embed);
         } else {
