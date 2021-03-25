d3a01c0eac8c1e5ba2a124203f64b9a1ca5869d1
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2021-03-25 18:15:20 +0100

    Update setwelcomech.js

--- commands/setup/setwelcomech.js
+++ commands/setup/setwelcomech.js
@@ -42,7 +42,7 @@ module.exports = {
       return message.channel.send(
         new MessageEmbed()
         .setColor('GREEN')
-        .setFooter(`Member Greeter | \©️${new Date().getFullYear()} Mai`)
+        .setFooter(`Member Greeter | \©️${new Date().getFullYear()} 𝕯𝖗𝖆𝖌𝖔𝖓𝖇𝖔𝖞#6241`)
         .setDescription([
           '<a:animatedcheck:758316325025087500>\u2000\u2000|\u2000\u2000',
           `Successfully set the welcome channel to ${channel}!\n\n`,
